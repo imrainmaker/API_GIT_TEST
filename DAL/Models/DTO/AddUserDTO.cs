7097dd8dbb58fd5548d4b76e5e32d736e17f5049
imrainmaker <bazinjeremy@gmail.com>
2023-04-26 16:23:28 +0200

    Ajout de la structure des DTO et ViewModel

---
+++ DAL/Models/DTO/AddUserDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models.DTO
+{
+    public class AddUserDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,25}$")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string CheckPassword { get; set; }
+    }
+}
